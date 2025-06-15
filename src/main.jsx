c665569407fca7798a4d55983ce570cd591582e5
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-06-15 15:41:51 -0500

    ye

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/Material.jsx
+++ src/pages/Material.jsx
@@ -1,5 +1,24 @@
-export default function Material(){
-    return (
-        <h1>Material</h1>
-    )
+import ButtonMT from "@/components/material-tailwind/Button";
+import Border from "@/components/Border";
+export default function Material() {
+  return (
+    <div className="p-4">
+      <h1>
+        Material Tailwind documentation:{" "}
+        <a
+          href="https://www.material-tailwind.com/docs/react/installation"
+          className="text-blue-950 underline"
+          target="_blank"
+        >
+          https://www.material-tailwind.com/docs/react/installation
+        </a>
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+        <Border>
+          <p className="text-center">Button</p>
+          <ButtonMT />
+        </Border>
+      </div>
+    </div>
+  );
 }
